e19dd33ca8c5e91c045e66b5b7f67b67115cb79e
Le Bon B' Bauma <baumabulondo@gmail.com>
2020-03-23 20:28:25 +0300

    Feat: Writing to the Response

--- Understanding Requests/app.js
+++
@@ -1,8 +0,0 @@
-const http = request('http');
-
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
-});
-
-server.listen(3000);
