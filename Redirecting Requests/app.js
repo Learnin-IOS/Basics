272916ab9869e4e43afb5a793d6cd588ef892c5e
Le Bon B' Bauma <baumabulondo@gmail.com>
2020-03-24 15:30:06 +0300

    get POST requests only

--- Redirecting Requests/app.js
+++ Redirecting Requests/app.js
@@ -1,13 +1,20 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    if (url==='/'){
+    const method = req.method
+    if (url ==='/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type= "text" name="message"><button type="Submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
+    }
+    if (url === '/message' && method==='POST' ){
+        fs.writeFile('message.txt');
+        
     }
     //process.exit();
     res.setHeader('Content-Type', 'text/html');
