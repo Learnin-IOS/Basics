4a2d65ba92fdc2e9ad8cedb3dfaffe230eeb978f
Le Bon B' Bauma <baumabulondo@gmail.com>
2020-03-26 22:38:10 +0300

    Getting data from Request URL and Request Method

--- Parsing Request Bodies/routes.js
+++ Parsing Request Bodies/routes.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
     if (url ==='/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
