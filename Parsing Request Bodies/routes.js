c60c8cb26beb647b3b2e33428d01f03f56138aec
Le Bon B' Bauma <baumabulondo@gmail.com>
2020-03-26 22:35:27 +0300

    Update route.js

--- Parsing Request Bodies/routes.js
+++ Parsing Request Bodies/routes.js
@@ -1,32 +1,34 @@
 const fs = require('fs');
-const requestHandler = () => {};
-if (url ==='/'){
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type= "text" name="message"><button type="Submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-}
-if (url === '/message' && method==='POST' ){
-    const body = [];
-    req.on('data', (chunk) =>{
-        console.log (chunk);
-        body.push(chunk);
-    });
-    return req.on('end', () =>{
-        const parseBody = Buffer.concat(body).toString();
-        console.log(parseBody);
-        const message = parseBody.split('=')[1];
-        fs.writeFile('message.txt', message, err =>{
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+const requestHandler = (req, res) => {
+    if (url ==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type= "text" name="message"><button type="Submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method==='POST' ){
+        const body = [];
+        req.on('data', (chunk) =>{
+            console.log (chunk);
+            body.push(chunk);
         });
+        return req.on('end', () =>{
+            const parseBody = Buffer.concat(body).toString();
+            console.log(parseBody);
+            const message = parseBody.split('=')[1];
+            fs.writeFile('message.txt', message, err =>{
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+            
         
-    
-    });        
-   
-}
+        });        
+       
+    }
+};
+
 res.setHeader('Content-Type', 'text/html');
 res.write('<html>');
 res.write('<head><title>My First Page</title></head>');
