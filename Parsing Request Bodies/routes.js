8288998cd308898589b82e4985b4733670fe1349
Le Bon B' Bauma <baumabulondo@gmail.com>
2020-03-26 22:41:09 +0300

    Exporting Requests form routes

--- Parsing Request Bodies/routes.js
+++ Parsing Request Bodies/routes.js
@@ -27,13 +27,17 @@ const requestHandler = (req, res) => {
             
         
         });        
+    
        
     }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1> Jambo Hapa form my node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+
 };
 
-res.setHeader('Content-Type', 'text/html');
-res.write('<html>');
-res.write('<head><title>My First Page</title></head>');
-res.write('<body><h1> Jambo Hapa form my node.js Server!</h1></body>');
-res.write('</html>');
-res.end();
+module.exports= requestHandler;
